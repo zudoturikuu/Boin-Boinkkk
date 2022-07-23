2cabb5e0090542a7d17754e36f8b8e57bb0df16b
Jun Heng <zudoturiku@duck.com>
2022-07-23 11:24:11 +0800

    help

--- Boin Boinkkk/BouncyView.swift
+++ Boin Boinkkk/BouncyView.swift
@@ -10,5 +10,18 @@ import SwiftUI
 import UIKit
 
 struct BouncyView: UIViewRepresentable {
+    
     var emojis: [String]
-}
+    var onCollide: ((CollisionSurface) -> Void)
+    
+    func makeUIView(context: Context) -> EmojiBouncyView {
+     return EmojiBouncyView(onCollide: onCollide)
+     }
+
+     func updateUIView(_ uiView: EmojiBouncyView, context: Context) {
+     if let last = emojis.last {
+     uiView.insert(emoji: last)
+     }
+     }
+    }
+
