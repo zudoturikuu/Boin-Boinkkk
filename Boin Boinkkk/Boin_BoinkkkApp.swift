e0143e25fed902514469b9ff6ec5482ed02a2535
Jun Heng <zudoturiku@duck.com>
2022-07-23 09:48:50 +0800

    Initial Commit

---
+++ Boin Boinkkk/Boin_BoinkkkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boin_BoinkkkApp.swift
+//  Boin Boinkkk
+//
+//  Created by Wong Jun heng on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Boin_BoinkkkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
