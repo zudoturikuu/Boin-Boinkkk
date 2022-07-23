c7a4645425d59d8806fb2d30dfd201df3a87a7f3
TristanZhu <oxraty@gmail.com>
2022-07-23 10:33:34 +0800

    haha

--- Boin Boinkkk/EmojiBouncyView.swift
+++ Boin Boinkkk/EmojiBouncyView.swift
@@ -31,3 +31,51 @@ class EmojiBouncyView: UIView, UICollisionBehaviourDelegate {
  fatalError("init(coder:) has not been implemented")
  }
 }
+func insert(emoji: String) {
+ let labelWidth = 80.0
+
+ let randX = Double.random(in: 0..<Double(bounds.width) - labelWidth)
+ let randY = Double.random(in: 0..<Double(bounds.height) - labelWidth)
+
+ let label = UILabel(frame: CGRect(x: randX, y: randY, width: labelWidth, height: labelWidth))
+
+ label.text = emoji
+ label.font = label.font.withSize(120)
+ label.adjustsFontSizeToFitWidth = true
+
+ label.backgroundColor = .systemGray5
+ label.layer.cornerRadius = 16
+ label.clipsToBounds = true
+ label.textAlignment = .center
+ self.addSubview(label)
+
+ collisions.addItem(label)
+ dynamics.addItem(label)
+
+ let push = UIPushBehavior(items: [label], mode: .instantaneous)
+ push.angle = CGFloat.random(in: 0 ..< .pi * 2)
+ push.magnitude = CGFloat(3.0 + .random(in: 0 ..< 1))
+ animator.addBehavior(push)
+
+ UIView.animate(withDuration: 2, delay: 5) {
+ label.alpha = 0
+ } completion: { _ in
+ label.removeFromSuperview()
+ push.removeItem(label)
+ self.dynamics.removeItem(label)
+ self.collisions.removeItem(label)
+ }
+ }
+}
+func collisionBehavior(_ behavior: UICollisionBehavior,
+ beganContactFor item1: UIDynamicItem,
+ with item2: UIDynamicItem, at p: CGPoint) {
+
+ }
+
+ func collisionBehavior(_ behavior: UICollisionBehavior,
+ endedContactFor item: UIDynamicItem,
+ withBoundaryIdentifier identifier: NSCopying?) {
+
+ }
+}
