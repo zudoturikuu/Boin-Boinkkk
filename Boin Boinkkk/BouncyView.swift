9ca74b7de5774961d144f00364baa2d356543e64
Jun Heng <zudoturiku@duck.com>
2022-07-23 11:08:08 +0800

    idk what i did

---
+++ Boin Boinkkk/BouncyView.swift
@@ -0,0 +1,14 @@
+//
+//  BouncyView.swift
+//  Boin Boinkkk
+//
+//  Created by Wong Jun heng on 23/7/22.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct BouncyView: UIViewRepresentable {
+    var emojis: [String]
+}
---
+++ Boin Boinkkk/CollisionSurface.swift
@@ -0,0 +1,13 @@
+//
+//  CollisionSurface.swift
+//  Boin Boinkkk
+//
+//  Created by Wong Jun heng on 23/7/22.
+//
+
+import Foundation
+
+enum CollisionSurface {
+    case emoji
+    case wall
+}
