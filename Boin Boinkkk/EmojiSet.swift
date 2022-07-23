21fffffa3a50b8ceb4678b2d1eb2732229f4e6d5
Jun Heng <zudoturiku@duck.com>
2022-07-23 09:55:17 +0800

    added emoji set struct

---
+++ Boin Boinkkk/EmojiSet.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiSet.swift
+//  Boin Boinkkk
+//
+//  Created by Wong Jun heng on 23/7/22.
+//
+
+import Foundation
+
+struct EmojiSet: Identifiable {
+    var id = UUID()
+    var emojis: [String]
+    var name: String
+    
+}
